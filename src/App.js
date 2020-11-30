1bed2260547d09cc6cada53f47cd2116ed7a2c54
Robertoreyes121 <rreyes5955@fdnyhsbk.org>
2020-11-30 19:15:15 +0000

    terminado

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>Roberto Reyes</h1>
+      <h1>favourite color black</h1>
+      <h1>I like rainy days</h1>
+      <h1>I like to see the snow fall but I don't like to walk on it</h1>
+      <h1>I was born in the Dominican Republic in the city of Santiago, but I moved to the city of New York when I was 11 years old, I am currently 15</h1>
+      <h1>I was born in 2004 December 19</h1>
     </div>
   );
 }
